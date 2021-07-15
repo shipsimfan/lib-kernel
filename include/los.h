408fb46e585304680265f4fb0a033c7a7417146e
Lance Hart <shipsimfan@hotmail.com>
2021-07-15 10:44:25 -0400

    Added keycodes

--- include/los.h
+++ include/los.h
@@ -4,6 +4,7 @@
 #include "los/console.h"
 #include "los/event.h"
 #include "los/filesystem.h"
+#include "los/keycode.h"
 #include "los/process.h"
 #include "los/syscall.h"
 #include "los/thread.h"
--- include/los/event.h
+++ include/los/event.h
@@ -3,9 +3,13 @@
 
 #include <stdint.h>
 
+#define EVENT_TYPE_KEY_PRESS 0
+#define EVENT_TYPE_KEY_RELEASE 1
+
 typedef struct {
     uint64_t type;
     uint64_t param1;
+    uint64_t param2;
 } Event;
 
 uint64_t peek_event(Event* pevent);
