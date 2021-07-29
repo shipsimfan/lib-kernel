c5c0cca14e34a26d6d18c7e3cd26e1459d6a909e
Lance Hart <shipsimfan@hotmail.com>
2021-07-29 14:08:47 -0400

    Added open flags

--- include/los/filesystem.h
+++ include/los/filesystem.h
@@ -11,13 +11,20 @@
 #define DIRENT_TYPE_DIRECTORY 0
 #define DIRENT_TYPE_FILE 1
 
+#define OPEN_READ 1
+#define OPEN_WRITE 2
+#define OPEN_READWRITE 3
+#define OPEN_TRUNCATE 4
+#define OPEN_APPEND 8
+#define OPEN_CREATE 16
+
 typedef struct {
     char name[256];
     size_t type;
     size_t size;
 } Dirent;
 
-int64_t open_file(const char* filepath);
+int64_t open_file(const char* filepath, uint64_t flags);
 void close_file(uint64_t fd);
 int64_t seek_file(uint64_t fd, uint64_t offset, uint64_t seek_from);
 int64_t read_file(uint64_t fd, void* buffer, uint64_t buffer_len);
--- src/filesystem/open_file.c
+++ src/filesystem/open_file.c
@@ -2,4 +2,4 @@
 
 #include <los/syscall.h>
 
-int64_t open_file(const char* filepath) { return system_call1(OPEN_FILE_SYSCALL, (uint64_t)filepath); }
+int64_t open_file(const char* filepath, uint64_t flags) { return system_call2(OPEN_FILE_SYSCALL, (uint64_t)filepath, flags); }
