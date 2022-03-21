62720ce76067efa766c348c695c156c0a836b5af
Lance Hart <shipsimfan@hotmail.com>
2022-03-21 18:18:59 -0400

    Added bool type

--- include/los/types.h
+++ include/los/types.h
@@ -3,9 +3,14 @@
 
 #include <stdint.h>
 
+#define false 0
+#define true 1
+
 typedef __INTPTR_TYPE__ isize;
 typedef __UINTPTR_TYPE__ usize;
 
 typedef __UINT8_TYPE__ u8;
+
+typedef usize bool;
 
 #endif
